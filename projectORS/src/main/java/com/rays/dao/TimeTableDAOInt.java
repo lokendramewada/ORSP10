a05cd8797dbd13ce4f208cf9518f7006172bae09
LOKENDRA SINGH <you@example.com>
2024-11-17 03:18:43 +0530

    ORS Project

---
+++ projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ * @author Lokendra Mewada
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
